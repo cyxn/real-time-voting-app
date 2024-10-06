4a8cce1e95e8aa321cb9abe0e4921a5f9cda0977
cyxn <nick.sukhov@eze.tech>
2024-10-06 10:37:26 +0200

    Updates

---
+++ static/js/198.d3782b5f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreal_time_voting_app=self.webpackChunkreal_time_voting_app||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.d3782b5f.chunk.js.map
