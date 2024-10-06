d6c51dcf964c38b558fe5e5d24e67a19ff7b1a67
cyxn <nick.sukhov@eze.tech>
2024-10-06 10:32:49 +0200

    add env variable for supabase client

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,7 @@
 // src/utils/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-// Replace these with your Supabase project credentials
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
